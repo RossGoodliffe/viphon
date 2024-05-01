8fc28e10ffe6c284df20b496680e15c666e9224f
RossGoodliffe <rossgoodliffe@gmail.com>
2024-05-01 14:04:53 +0100

    Update sendMessage.php

--- public/sendMessage.php
+++ public/sendMessage.php
@@ -21,24 +21,6 @@ unset($_POST['submit']);
 $stmt -> execute($criteria);
 
 
-// require_once('PHPMailer/PHPMailerAutoload.php');
-
-// $mail = new PHPMailer();
-// $mail->isSMTP();
-// $mail->SMTPAuth = true;
-// $mail->SMTPSecure = 'ssl';
-// $mail->Host = 'abbyandliam.co.uk';
-// $mail->Port = '465';
-// $mail->isHTML();
-// $mail->Username = "rsvp@abbyandliam.co.uk";
-// $mail->Password = "iwichswh123ABC";
-// $mail->SetFrom('rsvp@abbyandliam.co.uk');
-// $mail->Subject = "Abby and Liam | RSVP Confirmation";
-// $mail->Body = "Hello! Thank you for sending your RSVP. More details, such as your meal options, will be emailed to you nearer the time. Thank you!";
-// $mail->AddAddress($email);
-
-// $mail->Send();
-
 // Redirect to thank you page at the end
 header("Location: thankyou");
 
